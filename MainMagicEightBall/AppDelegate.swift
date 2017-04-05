bf2b760ee6bb31f0c90c50b7c8fd6e31bc8c8519
thomasknee <thomas.knee@shazam.com>
2017-04-05 17:13:15 +0100

    Initial Commit

---
+++ MainMagicEightBall/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MainMagicEightBall
+//
+//  Created by Thomas Knee on 05/04/2017.
+//  Copyright © 2017 Thomas Knee. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MainMagicEightBall/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MainMagicEightBall
+//
+//  Created by Thomas Knee on 05/04/2017.
+//  Copyright © 2017 Thomas Knee. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
